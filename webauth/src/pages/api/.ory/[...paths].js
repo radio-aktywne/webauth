e2d92af0d88671425d8042a656f59398bb9d8169
Sebastian Pietras <spietras1998@gmail.com>
2022-09-08 10:31:57 +0200

    Moved to HTTPS for `authgate` (#10)

--- webauth/src/pages/api/.ory/[...paths].js
+++ webauth/src/pages/api/.ory/[...paths].js
@@ -1,6 +1,6 @@
 import { config, createApiHandler } from "@ory/integrations/next-edge";
 
-const baseUrl = process.env.WEBAUTH_AUTHGATE_URL || "http://localhost:20000";
+const baseUrl = process.env.WEBAUTH_AUTHGATE_URL || "https://localhost:20000";
 const apiUrl = `${baseUrl}/authe`;
 
 export { config };
